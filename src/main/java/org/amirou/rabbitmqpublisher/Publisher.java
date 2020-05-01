c1932221a1403d27d73cbe7eacb52a575a39b4ce
Thierno Amirou Diallo <thierno.amirou.diallo@desjardins.com>
2020-05-01 17:57:03 -0400

    added Publisher

--- src/main/java/org/amirou/rabbitmqpublisher/Publisher.java
+++ src/main/java/org/amirou/rabbitmqpublisher/Publisher.java
@@ -1,4 +1,20 @@
 package org.amirou.rabbitmqpublisher;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Publisher {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()
+        ) {
+            String message = "First message to RabbitMQ";
+            channel.basicPublish("", "Queue-1", null, message.getBytes());
+        }
+    }
 }
